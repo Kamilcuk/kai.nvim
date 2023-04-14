914e9e872f337574ccdf715003249cf09d093157
Kamil Cukrowski <kamilcukrowski@gmail.com>
2023-04-14 16:02:08 +0200

    plugin: remove default mappings

--- plugin/ai.lua
+++ plugin/ai.lua
@@ -1,9 +1,3 @@
-vim.api.nvim_create_user_command("AI", function(args)
-    require("_ai/commands").ai(args)
-end, {range = true, nargs = "*"})
-
-if not vim.g.ai_no_mappings then
-    vim.api.nvim_set_keymap("n", "<C-a>", ":AI ", {noremap = true})
-    vim.api.nvim_set_keymap("v", "<C-a>", ":AI ", {noremap = true})
-    vim.api.nvim_set_keymap("i", "<C-a>", "<Esc>:AI<CR>a", {noremap = true})
-end
+vim.api.nvim_create_user_command("AI",
+                                 function(args) require("_ai/commands").ai(args) end,
+                                 {range = true, nargs = "*"})
